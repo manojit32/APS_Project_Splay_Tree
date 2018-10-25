5d09910cae824549f78866a0ae7e11cfc8f5818d
manojit32 <manojit270696@gmail.com>
2018-10-25 19:31:53 +0530

    Splay

---
+++ splay.py
@@ -0,0 +1,20 @@
+import bst
+
+class Splay(bst.BST):
+    def __init__(self):
+        super(Splay, self).__init__()
+
+    def _remove(self, z):
+        if z.left == None:
+            self.transplant(z, z.right)
+        elif z.right == None:
+            self.transplant(z, z.left)
+        else:
+            y = self.get_min(z.right)
+            if y.parent != z:
+                self.transplant(y, y.right)
+                y.right = z.right
+                y.right.parent = y
+            self.transplant(z, y)
+            y.left = z.left
+            y.left.parent = y
