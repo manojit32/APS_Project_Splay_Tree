aeeb7591f8ec4ac0e6646c6f499cdb59a114e5c2
manojit32 <manojit270696@gmail.com>
2018-10-25 20:50:55 +0530

    Added Search and Insert

--- splay.py
+++ splay.py
@@ -4,6 +4,31 @@ class Splay(bst.BST):
     def __init__(self):
         super(Splay, self).__init__()
 
+    def _insert(self, key, node):
+        if key < node.key:
+            if node.left:
+                self._insert(key, node.left)
+            else:
+                node.left = bst.Node(key, parent=node)
+                self.splay(node.left)
+        else:
+            if node.right:
+                self._insert(key, node.right)
+            else:
+                node.right = bst.Node(key, parent=node)
+                self.splay(node.right)
+
+    def _search(self, key, node):
+        if not node:
+            return False
+        elif key == node.key:
+            self.splay(node)
+            return True
+        elif key < node.key:
+            return self._search(key, node.left)
+        else:
+            return self._search(key, node.right)
+
     def _remove(self, z):
         if z.left == None:
             self.transplant(z, z.right)
